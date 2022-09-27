a3fee805db75235ebb3342fe28208639991de641
somogyik23 <112930860+somogyik23@users.noreply.github.com>
2022-09-27 09:05:57 +0200

    Add files via upload

---
+++ python/5_ora.py
@@ -0,0 +1,8 @@
+
+
+
+szám = input('Hogy hívnak?')
+szám = int(szám)
+print(type (szám))
+print(szám)
+
---
+++ python/print.py
@@ -0,0 +1,16 @@
+import os
+print('Hello, ' + os.getlogin() + '! How are you?')
+print('xx','xx')
+print('xx','xx', sep=)
+print()
+
+
+print("Somogyi Kristóf") 
+print('Somogyi Kristóf')
+print() 
+print(35) 
+print(3.67) 
+print(15 + 12) 
+print(5 == 5) 
+print(True) 
+print([1, 2, 4])

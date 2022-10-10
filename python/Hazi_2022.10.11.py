c765572c1959d1ea696739ae38db87faba7303a5
somogyik23 <112930860+somogyik23@users.noreply.github.com>
2022-10-10 19:10:13 +0200

    Add files via upload

---
+++ python/Hazi_2022.10.11.py
@@ -0,0 +1,7 @@
+szam1 = input('Kérem az első számot 1-Bármeddig: ')
+szam2 = input('Kérem a második számot 1-Bármeddig: ')
+szam1 = int(szam1)
+szam2 = int(szam2)
+print('A két szám szorzata:',szam1 * szam2)
+print('A két szám összege:',szam1 + szam2)
+print('A két szám különbsége:',szam1 - szam2)

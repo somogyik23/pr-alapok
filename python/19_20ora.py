c6e36c31f70f10564121a7832ee5b0be250c621e
somogyik23 <112930860+somogyik23@users.noreply.github.com>
2022-11-15 09:41:54 +0100

    Add files via upload

---
+++ python/19_20ora.py
@@ -0,0 +1,62 @@
+"""
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+"""
+# ----------------------------------------------------------
+"""
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+"""
+# ----------------------------------------------------------
+"""
+age = 36
+txt = "My name is John, I am " + age
+print(txt)
+"""
+# ----------------------------------------------------------
+"""
+print("ez\"egy\"sztring \n ez már egy új sor\titt egy tabulátor van")
+"""
+# ----------------------------------------------------------
+"""
+szam = 0 
+while szam != 100
+    szam = szam + 1
+    print(szam)
+print("A program vége!")
+"""
+# ----------------------------------------------------------
+"""
+szam = 0
+while szam != 100:
+    print(szam)
+    szam = szam + 1
+print("program vége!")
+"""
+# ----------------------------------------------------------
+"""
+szam = 0
+while szam != 101:
+    print(szam)
+    szam = szam + 1
+print("program vége!")
+"""
+# ----------------------------------------------------------
+"""
+szam = 1
+while True:
+    print(szam)
+    szam = szam + 1
+print("program vége!")
+"""
+# ----------------------------------------------------------
+"""
+szam = 0
+while szam != 100:
+    szam = szam + 1
+print("program vége!")
+"""
---
+++ python/HF_2022_11_15.py
@@ -0,0 +1,30 @@
+"""
+szam = 0
+while szam != 100:
+    print(szam)
+    szam = szam + 2
+print("program vége!")
+"""
+#--------------------------------------------------------
+"""
+szam = 1
+while szam != 103:
+    print(szam)
+    szam = szam + 3
+print("program vége!")
+"""
+#--------------------------------------------------------
+Páros
+"""
+szam = 0
+while szam != 102:
+    print(szam)
+    szam = szam + 2
+print("program vége!")
+"""
+#--------------------------------------------------------
+Páratlan
+#--------------------------------------------------------
+Házi feladat kiírása while utasítás segítségével
+szo = "Házi feladat"
+while szo != 1:

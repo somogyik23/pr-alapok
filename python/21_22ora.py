9789053bc9f78f5ae339d2fb75a9531f3048e3bd
somogyik23 <112930860+somogyik23@users.noreply.github.com>
2022-11-22 09:42:44 +0100

    Add files via upload

---
+++ python/21_22ora.py
@@ -0,0 +1,34 @@
+"""
+szam1 = 5
+szam2 = 6
+print(f"A szam1 + szam2 = {szam1 + szam2}")
+print(f"A szam1 / szam2 = {szam1 / szam2}")
+print(f"A szam1 / szam2 = {szam1 / szam2 : .2f}")
+print(f"A szam1 / szam2 = {szam1 / szam2 : .4f}")
+print(f"A szam1 / szam2 = {szam1 / szam2 : .1%}")
+print(f"A szam1 / szam2 = {szam1 / szam2 : .4%}")
+print(f"{1000000  :,}")
+print(f"{100000000 :,.2f}")
+
+print(f"A szam1 + szam2 = {szam1 + szam2} A szam1 - szam2 = {szam1 - szam2}")
+"""
+"""
+szam_lista = [3,5,9,2,44,1]
+for szam in szam_lista:
+    print(szam)
+
+szam_lista = [3,5,9,2,44,1]
+for szam in szam_lista:
+    print(szam, and=",")
+"""
+"""
+for szam1 in range(5):
+    print(szam1)
+"""
+#---------------------------------------------
+"""
+for szam2 in range(1,100):
+    print(szam2)
+"""
+
+szöveg = "Apple"

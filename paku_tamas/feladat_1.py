0e636cf4a721b0372643dd12e1b94f2d95d6a740
somogyik23 <112930860+somogyik23@users.noreply.github.com>
2023-01-17 09:21:52 +0100

    Add files via upload

---
+++ paku_tamas/feladat_1.py
@@ -0,0 +1,7 @@
+input(import(a="Adj meg egy számot!"))
+if (a==2%0)
+ print('A megadott szám pozitiv!')
+ if (a==0%2)
+ print("A megadott szám negatív!")
+ if (a==0)
+ print("A megadott szám nulla.")
---
+++ paku_tamas/feladat_2.py
@@ -0,0 +1,4 @@
+import(print(a='Adj meg szavakat! Ha kilépnél,a szó helyett csak egy ENTER-t üss!'))
+while print('Adj meg szavakat! Ha kilépnél,a szó helyett csak egy ENTER-t üss!')
+else:
+    

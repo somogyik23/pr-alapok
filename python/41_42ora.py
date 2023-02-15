804dade8f42a5215cd20c2b60f1fd9663b22d6cc
somogyik23 <112930860+somogyik23@users.noreply.github.com>
2023-02-15 07:56:24 +0100

    Add files via upload

---
+++ python/41_42ora.py
@@ -0,0 +1,14 @@
+#Somogyi kristóf
+
+szam = int(input("Kérek egy egész számot: "))
+
+if szam >= 1:
+    print("A megadott szám pozitív")
+
+elif szam <= -1:
+    print("A megadott szám negatív")
+
+else:
+    szam == 0
+    print("A megadott szám nulla")
+

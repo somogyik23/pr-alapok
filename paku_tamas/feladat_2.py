a21acb9a2c3e708ad81a552232b7bae53b38380e
somogyik23 <112930860+somogyik23@users.noreply.github.com>
2023-02-15 07:58:15 +0100

    Delete paku_tamas directory

--- paku_tamas/feladat_2.py
+++
@@ -1,4 +0,0 @@
-import(print(a='Adj meg szavakat! Ha kilépnél,a szó helyett csak egy ENTER-t üss!'))
-while print('Adj meg szavakat! Ha kilépnél,a szó helyett csak egy ENTER-t üss!')
-else:
-    
--- paku_tamas/feladat_3.py
+++
@@ -1,2 +0,0 @@
-import(print('honapok'))
-list:print('0 Január 1 Február 2 Március 3 Április 4 Május 5 Június')

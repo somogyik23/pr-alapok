8e1b5f7f85e9568ea14659ec95826125096000d3
somogyik23 <112930860+somogyik23@users.noreply.github.com>
2023-03-08 07:51:49 +0100

    Add files via upload

---
+++ python/Doga03_08.py
@@ -0,0 +1,6 @@
+# B csoport Somogyi Kristóf 
+
+szam_sorozat = ["15","4", "5", "19", "2"]
+szam_sorozat_egesz = [int(szam) for szam in szam_sorozat]
+minimum = min(szam_sorozat_egesz)
+print(minimum)

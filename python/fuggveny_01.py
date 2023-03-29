e43d42c9d453dbc9ce6704733401411e6ddab3e7
somogyik23 <112930860+somogyik23@users.noreply.github.com>
2023-03-29 08:28:24 +0200

    Add files via upload

---
+++ python/fuggveny_01.py
@@ -0,0 +1,9 @@
+#The ord() function returns the number representing the unicode code of a specified character.
+#The chr() function returns the character that represents the specified unicode.
+
+lista1 = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+print(min(lista1))
+print(max(lista1))
+print(lista1[2])
+print(lista1)
